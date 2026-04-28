fcb342c41a70afd5fae95f30e8819c38962b9219
Ashok Medasani <88182384+ashokmedasani@users.noreply.github.com>
2026-04-28 00:22:21 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,3 +5,16 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py bootstrap_admin
+
+
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser(
+        username="admin",
+        email="admin@example.com",
+        password="Admin@123"
+    )
+END
